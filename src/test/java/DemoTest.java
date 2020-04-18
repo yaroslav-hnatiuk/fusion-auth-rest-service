05c164936a061e7ca8f6de8f12edc37bb5e40648
yhnatiuk <yaroslavforjob@gmail.com>
2020-04-18 12:49:33 +0300

    Updated pom.xml and added logging in first.

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,12 +1,15 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Slf4j
 public class DemoTest {
     @Test
     public void demoTest() {
         boolean isDemo = true;
+        log.info("Demo test is running.");
         assertThat(isDemo, is(true));
     }
 }
